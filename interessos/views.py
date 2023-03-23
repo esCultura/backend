876abef93e865a6cff98097396ea469c8f16a76e
Pau Duran <pau.duran.manzano@upc.edu>
2023-03-23 12:44:38 +0100

    tasca #98 exemple de filter i ordering

--- interessos/views.py
+++ interessos/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import InteresEnEsdeveniment, InteresEnTematica
 from .serializers import InteresEnEsdevenimentSerializer, InteresEnTematicaSerializer
@@ -7,6 +8,9 @@ from .serializers import InteresEnEsdevenimentSerializer, InteresEnTematicaSeria
 class InteresEnEsdevenimentView(viewsets.ModelViewSet):
     queryset = InteresEnEsdeveniment.objects.all()
     serializer_class = InteresEnEsdevenimentSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['perfil', 'esdeveniment']
+    ordering_fields = ['perfil', 'esdeveniment']
 
 
 class InteresEnTematicaView(viewsets.ModelViewSet):
