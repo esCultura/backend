1465a2ce5cda26e82c1d81438e425420c3d44f12
xavierGarciaOrihuela <xavier.garcia.orihuela@estudiantat.upc.edu>
2023-03-24 23:34:21 +0100

    task #104: Filtratge i ordenació per paràmetres de les assistències

--- assistencies/views.py
+++ assistencies/views.py
@@ -1,4 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import AssistenciaAEsdeveniment
 from .serializers import AssistenciaAEsdevenimentSerializer
@@ -7,3 +12,6 @@ from .serializers import AssistenciaAEsdevenimentSerializer
 class AssistenciaAEsdevenimentView(viewsets.ModelViewSet):
     queryset = AssistenciaAEsdeveniment.objects.all()
     serializer_class = AssistenciaAEsdevenimentSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['perfil', 'esdeveniment']
+    ordering_fields = ['perfil', 'esdeveniment']
