233b75a2484f1cd8084e7a1997ba8d2a392d6ff3
xavierGarciaOrihuela <xavier.garcia.orihuela@estudiantat.upc.edu>
2023-03-24 23:30:50 +0100

    Filtratge i ordenació per paràmetres en els interessos de temàtiques

--- interessos/views.py
+++ interessos/views.py
@@ -26,9 +26,24 @@ class InteresEnEsdevenimentView(viewsets.ModelViewSet):
             interes = get_object_or_404(self.queryset, perfil=perfil, esdeveniment=esdeveniment)
             interes.delete()
             return Response(status=200, data={'message': f'S\'ha eliminat de forma correcta l\'interès del perfil {perfil} en l\'esdeveniment amb codi {esdeveniment}'})
-        return Response(status=500, data={'error': 'La Request ha de tenir dos paràmetres: perfil(username del perfil) i esdeveniment(codi de l\'esdeveniment)', 'perfil indicat': perfil, 'esdeveniment indicat': esdeveniment})
+        return Response(status=500, data={'error': 'La Request ha de tenir dos paràmetres: perfil (username del perfil) i esdeveniment (codi de l\'esdeveniment)', 'perfil indicat': perfil, 'esdeveniment indicat': esdeveniment})
 
 
 class InteresEnTematicaView(viewsets.ModelViewSet):
     queryset = InteresEnTematica.objects.all()
     serializer_class = InteresEnTematicaSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['perfil', 'tematica']
+    ordering_fields = ['perfil', 'tematica']
+
+    # Redefinició del mètode DELETE per tal de poder-lo realitzar a la ListView i a través de paràmetres, en aquest cas l'username i el codi de l'esdeveniment.
+    # (Les ListViews només suporten les operacions GET i POST, les DELETE requests només estan permeses a les DetailViews)
+    @action(methods=['delete'], detail=False)
+    def delete(self, request):
+        perfil = request.GET.get('perfil', None)
+        tematica = request.GET.get('tematica', None)
+        if(not (perfil is None or tematica is None)):
+            interes = get_object_or_404(self.queryset, perfil=perfil, tematica=tematica)
+            interes.delete()
+            return Response(status=200, data={'message': f'S\'ha eliminat de forma correcta l\'interès del perfil {perfil} en la temàtica {tematica}'})
+        return Response(status=500, data={'error': 'La Request ha de tenir dos paràmetres: perfil (username del perfil) i tematica (nom de la temàtica)', 'perfil indicat': perfil, 'temàtica indicada': tematica})
