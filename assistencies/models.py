5575ff9cd7577e4c324ecd6a7fa367c32bd8bf8d
Lehxugo <xavigo1234@gmail.com>
2023-04-02 18:41:10 +0200

    Afegit atribut data a assistència

--- assistencies/models.py
+++ assistencies/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from datetime import datetime
+
 # Create your models here.
 
 class AssistenciaAEsdeveniment(models.Model):
     perfil = models.ForeignKey('usuaris.Perfil', null=False, blank=False, on_delete=models.CASCADE, verbose_name=_('Username perfil'))
     esdeveniment = models.ForeignKey('esdeveniments.Esdeveniment', null=False, blank=False, on_delete=models.CASCADE, verbose_name=_('Codi esdeveniment'))
+    data = models.DateTimeField(null=False, blank=False, default=datetime.now, verbose_name=_('Data assistencia'))
 
     # Afegim una constraint per tal que no es pugui repetir
     # la combinació d'un perfil i un esdeveniment determinats.
