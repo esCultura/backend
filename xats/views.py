e94e52f678664c20d8b78c36753a67ed60e80151
Pau Duran <pau.duran.manzano@upc.edu>
2023-04-15 15:41:29 +0200

    task 155 filtratge de xats (id, nom, participants, dataCreació)

--- xats/views.py
+++ xats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Xat
 from .serializers import XatSerializer
 
@@ -8,3 +9,11 @@ class XatsView(viewsets.ModelViewSet):
     serializer_class = XatSerializer
     model = Xat
     permission_classes = []
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'id': ['exact', 'in'],
+        'nom': ['exact', 'in'],
+        'participants__username': ['in'],
+        'dataCreacio': ['exact', 'range'],
+    }
