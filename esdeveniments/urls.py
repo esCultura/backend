6666d0d5134b0bfa02df6dd85522221d4f0c9abc
Pau Duran <pau.duran.manzano@upc.edu>
2023-04-28 12:34:12 +0200

    task #204 inicialitzar tests d'esdeveniments

--- esdeveniments/urls.py
+++ esdeveniments/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.EsdevenimentsView)
+router.register(r'', views.EsdevenimentsView, basename='esdeveniments')
 
 urlpatterns = router.urls
