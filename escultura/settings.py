9bc03eb0997101ad534a0d9cf677c7aefda3f94a
Pau Duran <pau.duran.manzano@upc.edu>
2023-05-05 18:56:34 +0200

    DEMO DOCUMENTACIÓ (Codacy): Afegim un import innecessari a settings.py

--- escultura/settings.py
+++ escultura/settings.py
@@ -14,6 +14,8 @@ import sys
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 
+from . import urls
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
