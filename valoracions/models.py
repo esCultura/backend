3fe9cc365a74de66b36223ac30f4ead58607ad98
Pau Duran <pauduranmanzano@gmail.com>
2023-05-16 21:15:38 +0200

    task #261 ini valoracions i model Valoració

---
+++ valoracions/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from usuaris.models import Perfil
+from esdeveniments.models import Esdeveniment
+
+
+class Valoracio(models.Model):
+    TPuntuacio = (
+        (0, '0'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5')
+    )
+
+    id = models.AutoField(primary_key=True, verbose_name=_('Identidicador'))
+    data = models.DateTimeField(auto_now_add=True, verbose_name=_('Data creació'))
+    text = models.CharField(max_length=10000, verbose_name=_('Text'))
+    puntuacio = models.IntegerField(choices=TPuntuacio)
+    creador = models.ForeignKey(Perfil, related_name='valoracions', null=False, blank=False, on_delete=models.CASCADE, verbose_name=_('Creador'))
+    esdeveniment = models.ForeignKey(Esdeveniment, related_name='valoracions', null=False, blank=False, on_delete=models.CASCADE, verbose_name=_('Esdeveniment'))
