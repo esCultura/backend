dce6dee483b4c6c6ecd617e8e895c32bca93feba
Pau Duran <pauduranmanzano@gmail.com>
2023-05-17 08:55:05 +0200

    task #264 filtrar valoracions per esdeveniment (i usuari)

--- valoracions/views.py
+++ valoracions/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from usuaris import permissions
 from .models import Valoracio
 from .serializers import ValoracioSerializer
@@ -9,6 +10,12 @@ class ValoracionsView(viewsets.ModelViewSet):
     queryset = Valoracio.objects.all()
     serializer_class = ValoracioSerializer
     permission_classes = [permissions.IsPerfil]
+
+    filter_backends = [DjangoFilterBackend, ]
+    filterset_fields = {
+        'esdeveniment__codi': ['exact', 'in'],
+        'creador__user__id': ['exact', 'in']
+    }
 
     def create(self, request, *args, **kwargs):
         data = request.data.copy()
